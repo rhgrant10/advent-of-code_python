e4f2b8f458102b00e6826511e3f8705ceb9effcc
Robert Grant <rhgrant10@gmail.com>
2019-01-27 11:56:28 -0800

    2018, day 11 part 2 (touch up part 1)

--- aoc/problems/y2018_d11.py
+++ aoc/problems/y2018_d11.py
@@ -1,14 +1,10 @@
-# GRID_SERIAL_NO = 3628
-TOP = 1
-LEFT = 1
 
 
 def calculate_fuel_grid(serial_no, size):
-    num_columns, num_rows = size
     grid = []
-    for y in range(num_rows):
+    for y in range(size):
         row = []
-        for x in range(num_columns):
+        for x in range(size):
             power = get_power_level(x, y, serial_no)
             row.append(power)
         grid.append(row)
@@ -16,34 +12,71 @@ def calculate_fuel_grid(serial_no, size):
 
 
 def get_power_level(x, y, serial_no):
-    rack_id = (x + LEFT) + 10
-    power = rack_id * (y + TOP) + serial_no
+    rack_id = (x + 1) + 10
+    power = rack_id * (y + 1) + serial_no
     power *= rack_id
     hundreds = power // 100 % 10
     power = hundreds - 5
     return power
 
 
-def get_largest_total_power(grid, window):
+def find_max_location(grid, window):
     greatest = None
     top_left = None
-    wx, wy = window
-    for y in range(len(grid) - wy):
-        for x in range(len(grid[y]) - wx):
+    for y in range(len(grid) - window):
+        for x in range(len(grid[y]) - window):
             power = 0
-            for dy in range(wy):
-                for dx in range(wx):
+            for dy in range(window):
+                for dx in range(window):
                     power += grid[y + dy][x + dx]
 
-            if greatest is None or power > greatest:
+            if greatest is None or greatest < power:
                 greatest = power
                 top_left = x, y
-    x, y = top_left
-    return x + LEFT, y + TOP
+    return top_left
+
+
+def find_max_window(grid):
+    max_sum = top_left = size = None
+
+    length = len(grid)
+    for left in range(length):
+        dp = [0] * length
+        for right in range(left, length):
+            width = right - left + 1
+            for i in range(length):
+                dp[i] += grid[i][right]
+            sum_, start = find_max_sum(dp, width)
+            if max_sum is None or sum_ >= max_sum:
+                max_sum = sum_
+                top_left = left, start
+                size = width
+
+    return top_left, size
+
+
+def find_max_sum(arr, size):
+    max_sum = sum_ = sum(arr[:size])
+    start = 0
+    i = 0
+    while i < len(arr) - size:
+        sum_ += arr[size + i] - arr[i]
+        if sum_ > max_sum:
+            max_sum = sum_
+            start = i + 1
+        i += 1
+    return max_sum, start
 
 
 def part_1(input_):
     serial_number = int(input_.strip())
-    grid = calculate_fuel_grid(serial_number, size=(300, 300))
-    x, y = get_largest_total_power(grid, window=(3, 3))
-    return f'{x}, {y}'
+    grid = calculate_fuel_grid(serial_number, size=300)
+    x, y = find_max_location(grid, window=3)
+    return f'{x + 1},{y + 1}'
+
+
+def part_2(input_):
+    serial_number = int(input_.strip())
+    grid = calculate_fuel_grid(serial_number, size=300)
+    (x, y), w = find_max_window(grid)
+    return f'{x + 1},{y + 1},{w}'
